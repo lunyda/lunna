98c5e0d3bad60fb7b683108a00cf5fd6a90b8e01
lunyda <lalito.luna.ortix@gmail.com>
2026-08-04 16:14:56 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('http').createServer((req,res) => res.end('Bot Lunna funcionando')).listen(process.env.PORT || 3000);
 require('dotenv').config();
 const TelegramBotModule = require('node-telegram-bot-api');
 const TelegramBot = TelegramBotModule.default || TelegramBotModule;
